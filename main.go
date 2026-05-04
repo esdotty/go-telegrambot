1da1677941b19fc67359b51f04589728634cdb75
kate <kate@macs-MacBook-Pro.local>
2026-05-04 15:30:57 +0300

    Token parsing added

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	t := mustToken()
+	fmt.Println(t)
+
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New(tgClient)
+
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }
