3aee29d59b077a20a8bdff8ec20035d6d1230c32
kate <kate@macs-MacBook-Pro.local>
2026-05-12 11:00:21 +0300

    Added storage interface and methods to provide saving/removing data

---
+++ lib/storage/storage.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"crypto/sha1"
+	"io"
+
+	"github.com/esdotty/go-telegrambot-project/lib/e"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(UserName string) (*Page, error)
+	Remove(p *Page) error
+	IsExists(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL      string
+	UserName string
+}
+
+func (p Page) Hash() (string, error) {
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", e.Wrap("can't calculate hash", err)
+	}
+}
